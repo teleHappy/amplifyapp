7198c918bed2469c60282312c3d2c5a85364d3c7
telehappy <rick@rdillon.net>
2022-09-09 08:45:19 -0400

    init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React today
+          Learn React today or tomorrow
         </a>
       </header>
     </div>
